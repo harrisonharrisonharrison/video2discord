f2c05e2d28ac60f7e12f9f7dbd1e6d0ea95adc4e
Harrison Tran <harrit4@uci.edu>
2025-09-04 16:53:48 -0700

    drag icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 
 import './App.css'
 import TopBar from './assets/TopBar'
+import { AiFillPicture } from "react-icons/ai";
+
 
 function App() {
 
@@ -8,8 +10,9 @@ function App() {
     <>
       <TopBar/>
       
-      <div className='w-screen h-screen bg-gradient-to-b from-darple to-blurple flex justify-center'>
-        <div id="dropzone" className='border-white border-2 border-dashed w-[70vw] h-[40vh] bg-nqb rounded-3xl shadow-2xl flex justify-center items-center text-white text-3xl font-semibold'>
+      <div className='w-screen h-screen bg-gradient-to-b from-darple to-blurple flex justify-center p-10'>
+        <div id="dropzone" className='border-white border-2 border-dashed w-[70vw] h-[40vh] bg-nqb rounded-3xl shadow-2xl flex flex-col justify-center items-center text-white text-3xl font-semibold'>
+          <AiFillPicture className='text-9xl'/>
           Drag Files Here
         </div>
       </div>
